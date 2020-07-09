8d903fbbdafece7cde2f24a3ba380525a7309645
David <david@thejurne.com>
2020-07-09 12:09:22 -0700

    add db files

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log('Connected to DB...'))
-  .catch(err => console.log(err));
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+module.exports = app;
--- start.js
+++ start.js
@@ -1,5 +1,8 @@
 const app = require('./server');
+const dbConnect = require('./db');
 const PORT = process.env.PORT || 5000;
+
+dbConnect();
 
 app.listen(port, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
