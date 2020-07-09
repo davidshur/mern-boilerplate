d4bb1c733cb081fdf6013282a00ed19498d3ffcb
David <david@thejurne.com>
2020-07-09 12:27:08 -0700

    add server as main

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-const express = require('express');
-
+const express = require('express')
 const app = express();
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to DB...'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
