bf20eafd748b32fb25d17b5ef622294dfe0441e1
David <david@thejurne.com>
2020-07-14 13:54:46 -0700

    rm test

--- server.test.js
+++ server.test.js
@@ -3,14 +3,6 @@ const supertest = require('supertest');
 const request = supertest(app);
 
 describe('express routes', () => {
-  it('gets / endpoint', async done => {
-    const res = await request.get('/');
-
-    expect(res.status).toBe(200);
-    expect(res.body.message).toBe('Hello world!');
-    done();
-  });
-
   it('404 if route not found', async done => {
     const res = await request.get('/foo/bar');
 
