1605f40058c9e0cc9648ac7001fc4a15e508b4a7
David <david@thejurne.com>
2020-07-14 13:03:12 -0700

    add heroku postbuild

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello world!' });
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
